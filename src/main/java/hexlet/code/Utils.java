c5da592e23e77b40296b3b4f3b9c7d706aa70be1
proydemte <57496149+proydemte@users.noreply.github.com>
2021-08-06 21:35:28 +0300

    Specifying work of method

--- src/main/java/hexlet/code/Utils.java
+++ src/main/java/hexlet/code/Utils.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Utils {
     private static final Random RANDOM = new Random();
 
-    public static int random(int bound) {
-        return RANDOM.nextInt(bound);
+    public static int random(int boundExclusive) {
+        return RANDOM.nextInt(boundExclusive);
     }
 
-    public static int random(int startBound, int endBound) {
-        return startBound + RANDOM.nextInt(endBound) * 2;
+    public static int random(int startInclusive, int endExclusive) {
+        return startInclusive + RANDOM.nextInt(endExclusive) * 2;
     }
 }
